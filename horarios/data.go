a830a22a89637aeed70607c145649992cfdf7158
João Carlos Morgado David <joaodavid1999@hotmail.com>
2018-08-24 13:36:38 +0100

    Data agora tem tempo em vez de Horas

--- horarios/data.go
+++ horarios/data.go
@@ -1,8 +1,8 @@
 package horarios
 
 import (
-    "fmt"
-    "strconv"
+	"fmt"
+	"strconv"
 )
 
 // Dias da semana
@@ -18,70 +18,95 @@ const MININTERVALO = 30
 const HORA = 60
 
 type Hora struct {
-    hora, minuto int
-    val int
+	hora, minuto int
+	val          int
+}
+
+type Tempo struct {
+	inicio, fim int
 }
 
 type Data struct {
-    diaStr string
-    dia int
-    inicio *Hora
-    fim *Hora
+	diaStr string
+	dia    int
+	tempo  Tempo
+	// inicio *Hora
+	// fim    *Hora
 }
 
 // Inicializa a data, devolve o seu endereco.
 func NovaData(dataStr string) *Data {
-    data := &Data{}
-    data.dia = DiaStrParaInt(dataStr[0:3])
-    data.diaStr = DiaParaStr(data.dia)
-    data.inicio = NovaHora(dataStr[5:10])
-    data.fim = NovaHora(dataStr[15:20])
-    return data
+	data := &Data{}
+	data.dia = DiaStrParaInt(dataStr[0:3])
+	data.diaStr = DiaParaStr(data.dia)
+	data.tempo = NovoTempo(dataStr[5:10], dataStr[15:20])
+	// data.inicio = NovaHora(dataStr[5:10])
+	// data.fim = NovaHora(dataStr[15:20])
+	return data
 }
 
 // Devolve a representacao em int de um dia em string.
 func DiaStrParaInt(s string) int {
-    for i := 0; i < DIASSEMANA ; i++ {
-        if DiasPt[i] == s || DiasEn[i] == s {
-            return i
-        }
-    }
-    return DIASSEMANA
+	for i := 0; i < DIASSEMANA; i++ {
+		if DiasPt[i] == s || DiasEn[i] == s {
+			return i
+		}
+	}
+	return DIASSEMANA
 }
 
 // Devolve um dia como inteiro respetivo a string.
 func DiaParaStr(d int) string {
-    return DiasPt[d]
+	return DiasPt[d]
 }
 
 // Inicializa a hora com os dados obtidos da pagina da UC.
 func NovaHora(s string) *Hora {
-    h := &Hora{}
-    h.hora, _ = strconv.Atoi(s[0:2])
-    h.minuto, _ = strconv.Atoi(s[3:5])
-    h.val = horaInt(h)
-    return h
+	h := &Hora{}
+	h.hora, _ = strconv.Atoi(s[0:2])
+	h.minuto, _ = strconv.Atoi(s[3:5])
+	h.val = horaInt(h)
+	return h
+}
+
+func NovoTempo(inicio string, fim string) Tempo {
+	t := Tempo{}
+	horaInicio, _ := strconv.Atoi(inicio[0:2])
+	minutoInicio, _ := strconv.Atoi(inicio[3:5])
+	t.inicio = tempoInt(horaInicio, minutoInicio)
+	horaFim, _ := strconv.Atoi(fim[0:2])
+	minutoFim, _ := strconv.Atoi(fim[3:5])
+	t.fim = tempoInt(horaFim, minutoFim) - 1
+	return t
 }
 
 // Devolve True se d1 sobrepoe d2. False caso contrario.
 func (d1 *Data) Sobrepoe(d2 *Data) bool {
-    return (d1.dia == d2.dia ||
-           (d1.fim.val >= d2.inicio.val) ||
-           (d1.inicio.val <= d2.fim.val))
+	return (d1.dia == d2.dia ||
+		(d1.fim.val >= d2.inicio.val) ||
+		(d1.inicio.val <= d2.fim.val))
 }
 
 // Representacao em string de uma data.
 func (d *Data) String() string {
-    return fmt.Sprintf("%s, %v-%v", d.diaStr, d.inicio, d.fim)
+	return fmt.Sprintf("%d, %v", d.dia, d.tempo)
 }
 
 // Representacao em string de uma hora.
 func (h *Hora) String() string {
-    return fmt.Sprintf("%02d:%02d", h.hora, h.minuto)
+	return fmt.Sprintf("%02d:%02d - %d", h.hora, h.minuto, h.val)
 }
 
-// Devolve uma hora como um inteiro (8:00 = 0, 8:30 = 1, 9:00 = 2, etc.,
+func (t Tempo) String() string {
+	return fmt.Sprintf("%d-%d", t.inicio, t.fim)
+}
+
+func tempoInt(hora int, minuto int) int {
+	return (hora-TEMPOINICIO)*(HORA/MININTERVALO) + (minuto / MININTERVALO)
+}
+
+// Devolve uma hora como um inteiro (8:00-8:30 = 0, 8:30-9:00 = 1, etc.,
 // para TEMPOINICIO = 8 e MININTERVALO = 30)
 func horaInt(h *Hora) int {
-    return (h.hora - TEMPOINICIO)*(HORA/MININTERVALO) + (h.minuto/MININTERVALO)
+	return (h.hora-TEMPOINICIO)*(HORA/MININTERVALO) + (h.minuto / MININTERVALO)
 }
--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "./horarios"
+	"fmt"
+	"net/http"
+
+	"./horarios"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, "/Criador-Horarios", 301)
+	http.Redirect(w, r, "/Criador-Horarios", 301)
 }
 
 func horariosHandler(w http.ResponseWriter, r *http.Request) {
-    ucs := make([]*horarios.UnidadeCurricular, 0)
-    url := "https://fenix.tecnico.ulisboa.pt/disciplinas/FP4517957/2018-2019/1-semestre/pagina-inicial"
-    ucs = append(ucs, horarios.NovaUC(url))
-    fmt.Fprintf(w, "<h1>Simulador de Criador de Horarios</h1>")
-    fmt.Fprintf(w, "<p>%v</p>", ucs[0])
+	ucs := make([]*horarios.UnidadeCurricular, 0)
+	url := "https://fenix.tecnico.ulisboa.pt/disciplinas/FP4517957/2018-2019/1-semestre/pagina-inicial"
+	ucs = append(ucs, horarios.NovaUC(url))
+	fmt.Fprintf(w, "<h1>Simulador de Criador de Horarios</h1>")
+	fmt.Fprintf(w, "<p>%v</p>", ucs[0])
 }
 
 func main() {
-    http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/Criador-Horarios", horariosHandler)
-    http.ListenAndServe(":8000", nil)
+	// http.HandleFunc("/", indexHandler)
+	// http.HandleFunc("/Criador-Horarios", horariosHandler)
+	// http.ListenAndServe(":8000", nil)
+	nData := horarios.NovaData("Qui, 11:00 — 11:30")
+	fmt.Println(nData)
 }
