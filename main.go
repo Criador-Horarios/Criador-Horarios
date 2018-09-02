c86bf37b22bedd55ebbd599d92806547efdd2b39
João Carlos Morgado David <joaodavid1999@hotmail.com>
2018-09-02 18:50:32 +0100

    Changed folder structure

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -11,11 +9,6 @@ import (
 	"./schedules"
 	"github.com/gorilla/mux"
 )
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	page, _ := ioutil.ReadFile("index.html")
-	fmt.Fprint(w, string(page))
-}
 
 // getCU returns a JSON response of a CU. The format for the
 // CU id is NAME_LECTIVEYEAR_SEMESTER.
@@ -29,7 +22,7 @@ func getCU(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", indexHandler)
+	router.Handle("/", http.FileServer(http.Dir("./static")))
 	router.HandleFunc("/cus/{id}", getCU)
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
