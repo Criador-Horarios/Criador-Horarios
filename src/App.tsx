3c5485d05a8ff75008296db0bf23f5205f42d4fc
Daniel Gonçalves <daniel.gdf99@gmail.com>
2021-02-21 20:39:02 +0000

    Start new way of creating images from schedule

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,7 @@ import Backdrop from '@material-ui/core/Backdrop'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Divider from '@material-ui/core/Divider'
 import { exportComponentAsPNG } from 'react-component-export-image'
+import downloadAsImage from './utils/save-as-image'
 import Snackbar from '@material-ui/core/Snackbar'
 import Link from '@material-ui/core/Link'
 import GitHubIcon from '@material-ui/icons/GitHub'
@@ -432,13 +433,14 @@ class App extends React.Component <{
 			this.showAlert(i18next.t('alert.no-shift-selected'), 'info')
 			return
 		}
-		exportComponentAsPNG(this.chosenSchedule, {
-			fileName: 'ist-horario',
-			html2CanvasOptions: {
-				backgroundColor: undefined,
-				allowTaint: true,
-			}
-		})
+		// exportComponentAsPNG(this.chosenSchedule, {
+		// 	fileName: 'ist-horario',
+		// 	html2CanvasOptions: {
+		// 		backgroundColor: undefined,
+		// 		allowTaint: true,
+		// 	}
+		// })
+		downloadAsImage(this.state.selectedShifts)
 	
 		this.showAlert(i18next.t('alert.schedule-to-image'), 'success')
 	}
