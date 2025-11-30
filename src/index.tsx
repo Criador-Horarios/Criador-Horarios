229ead738067ac0f87e1ee5231c8ef939a045303
Daniel Gonçalves <daniel.gdf99@gmail.com>
2025-11-30 12:37:48 +0000

    remove: Web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App'
-// import reportWebVitals from './reportWebVitals'
 import API from './utils/api'
 import i18n from './i18n/i18n'
 import { I18nextProvider } from 'react-i18next'
@@ -26,8 +25,3 @@ root.render(
 		</I18nextProvider>
 	</React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
